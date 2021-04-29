d38f4c034875433a314339e44a3c38a6405cabfe
ATB_NguyenVanDat <nguyen.van.dat@alliedtechbase.com>
2021-04-29 16:32:04 +0900

    validate sensor measure

--- app/Http/Requests/StoreSensorMeasureRequest.php
+++ app/Http/Requests/StoreSensorMeasureRequest.php
@@ -5,6 +5,8 @@ namespace App\Http\Requests;
 use App\Rules\CheckSensorSettingRelationRule;
 use App\Rules\CheckSettingRelationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
 
 class StoreSensorMeasureRequest extends FormRequest
 {
@@ -35,6 +37,36 @@ class StoreSensorMeasureRequest extends FormRequest
             $rules['sensor_setting_id'] = ['required', 'numeric', 'min:1'];
         }
 
+        if (!empty($this->get('measba'))) {
+            $rules['measba.datetime'] = 'required';
+            $rules['measba.pastaerr'] = 'required|numeric';
+        }
+
+        if (!empty($this->get('measpara'))) {
+            $rules['measpara.setname'] = 'required|string|max:50';
+            $rules['measpara.bacs'] = 'required|numeric';
+            $rules['measpara.crng'] = 'required|numeric';
+            $rules['measpara.eqp1'] = 'required|numeric';
+        }
+
+        if (!empty($this->get('measdet'))) {
+            $rules['measdet.rawdmp'] = 'required|array';
+        }
+
+        if (!empty($this->get('measres'))) {
+            $rules['measres.name'] = 'required|string|max:50';
+            $rules['measres.pkpot'] = 'required|numeric';
+            $rules['measres.dltc'] = 'required|numeric';
+            $rules['measres.bgc'] = 'required|numeric';
+            $rules['measres.err'] = 'required|numeric';
+        }
+
         return $rules;
+    }
+
+    protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
+    {
+        $response = new Response(['errors' => $validator->errors()], 422);
+        throw new ValidationException($validator, $response);
     }
 }
