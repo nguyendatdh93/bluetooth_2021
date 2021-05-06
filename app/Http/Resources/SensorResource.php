96f553e89f343ba2c297768e74437a9922577c8a
ATB_NguyenVanDat <nguyen.van.dat@alliedtechbase.com>
2021-05-06 15:02:31 +0900

    api resource response only id when save sensor info

--- app/Http/Resources/SensorResource.php
+++ app/Http/Resources/SensorResource.php
@@ -14,6 +14,8 @@ class SensorResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+        ];
     }
 }
