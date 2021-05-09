0caefc5b4861307bf9ed37046ca03cff7a84e487
nguyendatdh93 <nguyendatdh93@gmail.com>
2021-05-09 18:32:05 +0700

    json measpara bac

--- app/Http/Resources/MeasparaResource.php
+++ app/Http/Resources/MeasparaResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use function PHPUnit\Framework\isJson;
 
 class MeasparaResource extends JsonResource
 {
@@ -14,6 +15,17 @@ class MeasparaResource extends JsonResource
      */
     public function toArray($request)
     {
-        return json_decode($this->settings);
+        $data = json_decode($this->settings);
+        if ($this->isJsonString($data->bac)) {
+            $data->bac = json_decode($data->bac);
+        }
+
+        return $data;
+    }
+
+    private function isJsonString($string)
+    {
+        json_decode($string);
+        return (json_last_error() == JSON_ERROR_NONE);
     }
 }
